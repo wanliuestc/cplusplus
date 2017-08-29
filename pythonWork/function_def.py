b27a48efe390a3304c559298a8d79185829c8665
wanliuestc <changwanli777@163.com>
2017-08-29 21:33:30 +0800

    2017-8-29 21:33

--- pythonWork/function_def.py
+++ pythonWork/function_def.py
@@ -38,3 +38,49 @@ print(test())
 print(test())
 print(test())
 print(test())
+
+print('-----------------------------------------------')
+
+def person(name, age, **kw):
+    print('name:', name, 'age:', age, 'other:', kw)
+
+person('hello', 12, **{'hello':'world', '777':'999'})
+
+list1 = [1,2,3,4]
+if 1 in list1 :
+    print('yes')
+else:
+    print('no')
+
+dict1 = {'name':'changwanli', 'age':15}
+if 'changwanli' in dict1:
+    print('yes')
+else:
+    print('nono')
+ 
+for t in dict1:
+    print(dict1[t])
+
+def person2(name, age, *, city, job):
+    print(name, age, city, job)
+
+person2('changwanli', 100, city='huainan', job='killer')
+person2('changwanli', 100, **{'city':'huainan', 'job':'killer'})
+
+
+def f1(a, b, c=0, *args, **kw):
+    print('a=', a, 'b=', b, 'c=', c, 'args=', args, 'kw=', kw)
+
+def f2(a, b, c=0, *, d, kw):
+    print('a=', a, 'b=', b, 'c=', c, 'd=', d, 'kw=', kw)
+
+
+f1(1, 2)
+f1(1, 2, 3)
+f1(1, 2, 3, 'a', 'b')
+f1(1, 2, 3, 'a', 'b', x=99)
+f1(1, 2, 3, 'a', 'b', x=99, y=100)
+
+f2(1, 2, 3, d='tt', kw='meng')
+f2(*(1,2,3), **{'d':'lisi', 'kw':13})
+f2(*[1,2,3], **{'d':'lisi', 'kw':13})
---
+++ pythonWork/generate.py
@@ -0,0 +1,15 @@
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n+1
+    return 'done'
+
+fib(4)
+
+f = fib(4)
+f
+
+for t in fib(4):
+    print(t)
